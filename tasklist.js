bd148418a58473023f4781fd33ccaba314c002aa
Irishgirl <Irishgirl@users.noreply.github.com>
2014-05-06 19:42:37 -0700

    Update tasklist.js

--- tasklist.js
+++ tasklist.js
@@ -9,7 +9,7 @@ var taskList = function taskList (spec) {
 
     clearAll: function clearAll () {
       localStorage.clear();
-      that.tasks = [];
+      spec.tasks = [];
       return [];
     },
     
@@ -20,17 +20,17 @@ var taskList = function taskList (spec) {
     },
   
     remove: function remove (taskId) {
-      var removed = that.tasks.splice(taskId, 1);
+      var removed = spec.tasks.splice(taskId, 1);
       localStorage.setItem(that.name, JSON.stringify(that.tasks));
       return (removed.length > 0) ? true: false;
     },
 
     get: function get (taskId) {
-      return that.tasks[taskId];
+      return spec.tasks[taskId];
     },
   
     getAll: function getAll (taskId) {
-      return that.tasks;
+      return spec.tasks;
     },
 
     getName: function getName () {
@@ -38,7 +38,7 @@ var taskList = function taskList (spec) {
     },
     
     find: function find (task) {
-      return that.tasks.indexOf(task);
+      return spec.tasks.indexOf(task);
     },
     
     search: function search (pattern, flags) {
@@ -49,13 +49,13 @@ var taskList = function taskList (spec) {
       if (! pattern instanceof RegExp) {
         return [];
       } else {
-        return that.tasks.filter(function (task) {
+        return spec.tasks.filter(function (task) {
           return pattern.test(task.name) || pattern.test(task.details);
         });
       }
     }
   },
-  localItems = localStorage.getItem(that.name),
+  localItems = localStorage.getItem(spec.name),
   tmpTasks;
 
   localStorage.setItem(that.name + ' [VERSION]', 1);
@@ -63,14 +63,14 @@ var taskList = function taskList (spec) {
   if (localItems) {
     tmpTasks = JSON.parse(localItems);
     if (tmpTasks instanceof Array) {
-      that.tasks = tmpTasks;
+      spec.tasks = tmpTasks;
     }
   }
 
   if (typeof Array.prototype.forEach === 'function') {
     // real browser, or IE >=9
     that.forEach = function forEach (fn) {
-      that.tasks.forEach(fn);
+      spec.tasks.forEach(fn);
     };
   } else {
     // last resort - fake it
